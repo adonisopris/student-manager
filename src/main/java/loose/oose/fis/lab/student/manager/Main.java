b62df7376ddeedad43d2009f647256f63e510df0
Marius Murariu <108189379+MurariuMarius@users.noreply.github.com>
2023-04-01 16:06:59 +0300

    Update src/main/java/loose/oose/fis/lab/student/manager/Main.java
    Changed the application title to contain the company name
    
    Co-authored-by: adonisopris <128553327+adonisopris@users.noreply.github.com>

--- src/main/java/loose/oose/fis/lab/student/manager/Main.java
+++ src/main/java/loose/oose/fis/lab/student/manager/Main.java
@@ -4,7 +4,7 @@ public class Main {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
-        primaryStage.setTitle("Student Manager");
+        primaryStage.setTitle("FIS - Student Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
